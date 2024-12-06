9d77c0107693fb46b36ae523b34af61c2d8be59d
Novaliyn <189510880+Novaliyn@users.noreply.github.com>
2024-12-06 15:23:59 -0800

    added GUI for a selection menu

--- Different-flags.py
+++ Different-flags.py
@@ -1,34 +1,55 @@
 from tkinter import *
 
+#Convert button clicks into variables.
+def button_click(id):
+    global flag
+    flag = int(id)
+    update()
+
+#Clears canvas.
+def update():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+#created the flag and shapes.
+    if flag == 0:
+        screen = Canvas(root, width=400, height=300, bg="red")
+        #shapes
+        screen.create_polygon(0,0,200,150,80,0,0,0, fill="yellow")
+        screen.create_polygon(175,0,200,150,225,0,175,0, fill="yellow")
+        screen.create_polygon(400,0,200,150,320,0,400,0, fill="yellow")
+        screen.create_polygon(0,125,200,150,0,175,0,125, fill="yellow")
+        screen.create_polygon(0,300,200,150,80,300,0,300, fill="yellow")
+        screen.create_polygon(175,300,200,150,225,300,175,300, fill="yellow")
+        screen.create_polygon(400,300,200,150,320,300,400,300, fill="yellow")
+        screen.create_polygon(400,125,200,150,400,175,400,175, fill="yellow")
+        screen.create_oval(170,120,230,180, fill="yellow", outline="")
+    elif flag == 1:
+        screen = Canvas(root, width=400, height= 300, bg="white")
+        screen.create_rectangle(0,150,400,300, fill="red")
+    elif flag == 2:
+        screen = Canvas(root, width=640, height=480, background="#00f0ff")
+        screen.create_rectangle(0,96,640,192, fill="#ff5bf3", outline="")
+        screen.create_rectangle(0,192,640,288, fill="white", outline="")
+        screen.create_rectangle(0,288,640,384, fill="#ff5bf3", outline="")
+    elif flag == 3:
+        screen =  Canvas(root, width=640, height=480, bg ="white")
+        screen.create_rectangle(0,0,640,96, fill="#ef9007", outline="")
+        screen.create_rectangle(0,96,640,192, fill="#f6d317", outline="")
+        screen.create_rectangle(0,288,640,384, fill="#45bcee", outline="")
+        screen.create_rectangle(0,384,640,480, fill="#1e3f54", outline="")
+    else:
+        root.quit()
+    screen.pack()
+
+#Setup
+flag=-1
 root = Tk()
-flags = ["North Macedonia (NM)","Poland (P)", "Secret (T)", "Secret2 (AA)"]
-print(flags)
-flag = input("Please select the flag you would like to print: ")
-if flag == "NM":
-    screen = Canvas(root, width=400, height=300, bg="red")
-    #shapes
-    screen.create_polygon(0,0,200,150,80,0,0,0, fill="yellow")
-    screen.create_polygon(175,0,200,150,225,0,175,0, fill="yellow")
-    screen.create_polygon(400,0,200,150,320,0,400,0, fill="yellow")
-    screen.create_polygon(0,125,200,150,0,175,0,125, fill="yellow")
-    screen.create_polygon(0,300,200,150,80,300,0,300, fill="yellow")
-    screen.create_polygon(175,300,200,150,225,300,175,300, fill="yellow")
-    screen.create_polygon(400,300,200,150,320,300,400,300, fill="yellow")
-    screen.create_polygon(400,125,200,150,400,175,400,175, fill="yellow")
-    screen.create_oval(170,120,230,180, fill="yellow", outline="")
-elif flag == "P":
-    screen = Canvas(root, width=400, height= 300, bg="white")
-    screen.create_rectangle(0,150,400,300, fill="red")
-elif flag == "T":
-    screen = Canvas(root, width=640, height=480, background="#00f0ff")
-    screen.create_rectangle(0,96,640,192, fill="#ff5bf3", outline="")
-    screen.create_rectangle(0,192,640,288, fill="white", outline="")
-    screen.create_rectangle(0,288,640,384, fill="#ff5bf3", outline="")
-elif flag == "AA":
-    screen =  Canvas(root, width=640, height=480, bg ="white")
-    screen.create_rectangle(0,0,640,96, fill="#ef9007", outline="")
-    screen.create_rectangle(0,96,640,192, fill="#f6d317", outline="")
-    screen.create_rectangle(0,288,640,384, fill="#45bcee", outline="")
-    screen.create_rectangle(0,384,640,480, fill="#1e3f54", outline="")
-screen.pack()
+flags = ["North Macedonia","Poland", "Secret", "Secret2","Exit"]
+
+#display buttons
+for i in range(len(flags)):
+    button = Button(root, text=flags[i], command= lambda i = i: button_click(i))
+    button.pack(pady=5)
+
 mainloop()
